d8059243e10f9b5e2361e1f5c94738877942f8fe
jayaaiyappan <34357780+jayaaiyappan@users.noreply.github.com>
2022-04-30 16:41:33 +0530

    changes

---
+++ copy_to_hdfs/src/main/java/copyToHDFS.java
@@ -0,0 +1,32 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+
+import java.io.IOException;
+
+public class copyToHDFS {
+    public static void main(String[] args) {
+
+        System.out.println(System.getProperty("userid"));
+        String hdfsSiteXML = "/home/ubuntu/hadoop/etc/hadoop/hdfs-site.xml";
+        String coreSiteXML = "/home/ubuntu/hadoop/etc/hadoop/core-site.xml";
+        String localfilepath = args[0];
+        String hdfstargetpath = args[1];
+
+        Configuration conf = new Configuration();
+        conf.addResource(new Path(hdfsSiteXML));
+        conf.addResource(new Path(coreSiteXML));
+
+        try {
+            FileSystem fs = FileSystem.get(conf);
+            fs.copyFromLocalFile(false, true, new Path(localfilepath), new Path(hdfstargetpath));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+            System.out.println(System.getProperty("inside catch"));
+        }
+
+    }
+
+}
